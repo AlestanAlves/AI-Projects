4cfb87e6bd6f0909e6ed1a2b6ce4f5f98a2d5c44
AlestanAlves <alestanalves@gmail.com>
2022-02-26 23:45:56 -0300

    edit tracking

--- project1_video_emotion/test2.py
+++ project1_video_emotion/test2.py
@@ -2,13 +2,13 @@ import cv2
 from deepface import DeepFace
 import numpy as np
 
-face_cascade = csv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-video = csv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+video = cv2.VideoCapture(0)
 
 while video.isOpened():
     _,frame = video.read()
 
-    gray = csv2.cstColor(frame, csv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
     for x,y,w,h in face:
@@ -16,12 +16,11 @@ while video.isOpened():
         try:
             analyze = DeepFace.analyze(frame, actions=["emotion"])
             print(analyze('dominant_emotion'))
-
         except:
             print("no face")
 
-    csv2.inshow("video", frame)
-    key= csv2.waitKey(1)
+    cv2.imshow("video", frame)
+    key= cv2.waitKey(1)
     if key==ord("q"):
         break
 
